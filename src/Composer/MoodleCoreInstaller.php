7fc29ee7274e0abcc5450e8743f4436efb3a0fd3
Drake <drake@borrellassociates.com>
2023-01-26 11:36:56 -0500

    update installer logic

--- src/Composer/MoodleCoreInstaller.php
+++ src/Composer/MoodleCoreInstaller.php
@@ -8,24 +8,35 @@ use Composer\Package\PackageInterface;
 
 class MoodleCoreInstaller extends LibraryInstaller
 {
+    const TYPE = 'moodle-core';
+    const DEFAULT_DIR = 'moodle';
+    const INSTALL_META_KEY = 'moodle-install-dir';
+
+    const MESSAGE_CONFLICT = 'Two packages (%s and %s) cannot share the same directory!';
+    const MESSAGE_SENSITIVE = 'Warning! %s is an invalid Moodle install directory (from %s)!';
+
+    private static $_installedPaths = array();
+
+    private $sensitiveDirectories = array( '.' );
+
     public function getInstallPath( PackageInterface $package ) {
         $installationDir = false;
         $prettyName      = $package->getPrettyName();
         if ( $this->composer->getPackage() ) {
             $topExtra = $this->composer->getPackage()->getExtra();
-            if ( ! empty( $topExtra['moodle-install-dir'] ) ) {
-                $installationDir = $topExtra['moodle-install-dir'];
+            if ( ! empty( $topExtra[self::INSTALL_META_KEY] ) ) {
+                $installationDir = $topExtra[self::INSTALL_META_KEY];
                 if ( is_array( $installationDir ) ) {
                     $installationDir = empty( $installationDir[ $prettyName ] ) ? false : $installationDir[ $prettyName ];
                 }
             }
         }
         $extra = $package->getExtra();
-        if ( ! $installationDir && ! empty( $extra['moodle-install-dir'] ) ) {
-            $installationDir = $extra['moodle-install-dir'];
+        if ( ! $installationDir && ! empty( $extra[self::INSTALL_META_KEY] ) ) {
+            $installationDir = $extra[self::INSTALL_META_KEY];
         }
         if ( ! $installationDir ) {
-            $installationDir = 'moodle';
+            $installationDir = self::DEFAULT_DIR;
         }
         $vendorDir = $this->composer->getConfig()->get( 'vendor-dir', Config::RELATIVE_PATHS ) ?: 'vendor';
         if (
@@ -44,5 +55,17 @@ class MoodleCoreInstaller extends LibraryInstaller
         self::$_installedPaths[ $installationDir ] = $prettyName;
 
         return $installationDir;
+    }
+
+    public function supports( $packageType ) {
+        return self::TYPE === $packageType;
+    }
+
+    private function getConflictMessage( $attempted, $alreadyExists ) {
+        return sprintf( self::MESSAGE_CONFLICT, $attempted, $alreadyExists );
+    }
+
+    private function getSensitiveDirectoryMessage( $attempted, $packageName ) {
+        return sprintf( self::MESSAGE_SENSITIVE, $attempted, $packageName );
     }
 }
